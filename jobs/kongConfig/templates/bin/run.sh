c35e5b49dc80cb3fe46f8d27699cafffd583d20c
Alex <thexanderproject@gmail.com>
2017-12-12 10:29:20 -0500

    Errand for updating config to kong's admin api working

--- jobs/kongConfig/templates/bin/run.sh
+++ jobs/kongConfig/templates/bin/run.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
 curl -i -X POST \
-  --url http://<%= link('kong').p('kong_admin_listen') %>/apis/ \
-  --data 'name=<<%= p("name") %>' \
-  --data 'hosts=<<%= p("hosts") %>' \
-  --data 'upstream_url=<<%= p("upstream_url") %>'
+  --url http://<%= link('kong').instances[0].address %>:<%= link('kong').p('kong_admin_listen_port') %>/apis/ \
+  --data 'name=<%= p("name") %>' \
+  --data 'hosts=<%= p("hosts") %>' \
+  --data 'upstream_url=<%= p("upstream_url") %>'
+  exit 0
