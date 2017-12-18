54e8842d4f45595fc05dbaa2482753dddbd204b0
Alex <thexanderproject@gmail.com>
2017-12-18 11:45:56 -0500

    changed configuration setup to be more modular/expandable.  started to implement wiping of kong config on the errand

--- jobs/kongConfig/templates/bin/run.sh
+++ jobs/kongConfig/templates/bin/run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-curl -i -X POST \
-  --url http://<%= link('kong').instances[0].address %>:<%= link('kong').p('kong_admin_listen_port') %>/apis/ \
+export KONG_ADMIN_ADDRESS=http://<%= link('kong').instances[0].address %>:<%= link('kong').p('kong_admin_listen_port') %>
+export KONG_ADMIN_RESPONSE=$(curl ${KONG_ADMIN_ADDRESS}/apis/)
+
+export NUM_OF_MAPPINGS=$(echo ${KONG_ADMIN_RESPONSE} | jq '.[0] | .total')
+
+#delete existing kong mappings
+for i in `seq 1 10`;
+  do
+    echo $i
+  done
+
+#send the new mappings to the kong admin
+curl POST \
+  --url ${KONG_ADMIN_ADDRESS}/apis/ \
   --data 'name=<%= p("name") %>' \
   --data 'hosts=<%= p("hosts") %>' \
-  --data 'upstream_url=<%= p("upstream_url") %>'
+  --data 'upstream_url=<%= p("upstream_url") %>
   exit 0
