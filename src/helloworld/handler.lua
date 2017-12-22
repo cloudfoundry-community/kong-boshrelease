decc00ef87f30bde0f3f3bafa66b6ecb2ab63a8c
Alex <thexanderproject@gmail.com>
2017-12-22 08:25:27 -0500

    cherrypicked long's plugin changes

---
+++ src/helloworld/handler.lua
@@ -0,0 +1,15 @@
+local BasePlugin = require "kong.plugins.base_plugin"
+local access = require "kong.plugins.helloworld.access"
+
+local HelloWorldHandler = BasePlugin:extend()
+
+function HelloWorldHandler:new()
+  HelloWorldHandler.super.new(self, "helloworld")
+end
+
+function HelloWorldHandler:access(conf)
+  HelloWorldHandler.super.access(self)
+  access.execute(conf)
+end
+
+return HelloWorldHandler
