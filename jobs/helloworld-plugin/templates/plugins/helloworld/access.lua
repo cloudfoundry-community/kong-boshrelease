67495f7a73e9b5ff83b84d7d97927ce12a4bed81
Alex <thexanderproject@gmail.com>
2018-01-05 12:07:34 -0500

    add in token grabbing for proper user auth

--- jobs/helloworld-plugin/templates/plugins/helloworld/access.lua
+++ jobs/helloworld-plugin/templates/plugins/helloworld/access.lua
@@ -16,19 +16,18 @@ function _M.execute(conf)
 
   ]]--
 
-j
   --[[
     1. convert space guid to org guid
-    cf m from cli
-    incoming request: 
+    command being intercepted is `cf m`
+    `cf m` sends an incoming request: 
       GET /v2/spaces/:guid/services
 
     create a request to:
       GET /v2/spaces/:guid
-    to get organization_guid
+    to get the organization_guid for the given space
     ]]--
   local http = require("socket.http")
-  local rawHeader = ngx.req.raw_header()
+  --local rawHeader = ngx.req.raw_header()
 
   --regext to check if the request is sent to the uri that we're listening for (with a guid in the middle there)
   local uri_location_matches, err = ngx.re.match(ngx.var.uri, "/v2/spaces/[0-9a-fA-F]{8}\-([0-9a-fA-F]{4}\-){3}[0-9a-fA-F]{12}/services")
@@ -41,19 +40,30 @@ j
   end
 
   -- if the request is of the type we're looking for (a GET request to /v2/spaces/:guid/services),
-  -- then we can ping the spaces endpoint to get the guid we care about (org guid in this case)
+  -- then we can ping the cf api spaces endpoint to get the guid we care about (org guid in this case)
   if (ngx.req.get_method() == "GET") and uri_location_matches then
     space_guid = string.match(ngx.req.ui, "*/v2/spaces/(*)/services")
-    result, statuscode, content = http.request("https://api.system.aws-usw02-sb.ice.predix.io/v2/spaces/" .. space_guid)
-    -- local org_guid = content.organization_guid
+    
+    -- grab the UAA token from the request
+    local auth_token = ngx.req.get_headers()["Authorization"]
+    result, content = http.request {
+      url = "https://api.system.aws-usw02-sb.ice.predix.io/v2/spaces/" .. space_guid,
+      headers = { Authorization = auth_token }
+    }
+
+    --local org_guid = content.organization_guid
     ngx.print("result:  " .. result)
     ngx.print("statuscode:  " .. statuscode)
     ngx.print("content:  " .. content)
   end
-  ngx.pring("raw header:  " .. ngx.req.raw_header())
+  ngx.print("raw header:  " .. ngx.req.raw_header())
 
 -- 2. get all services from Nurego
 -- GET /v1/catalog on Nurego endpoint to get services
+nurego_result, nurego_content = http.request {
+  url = "https://nurego.endpoint/v1/catalog",
+  headers = { Authorization = auth_token }
+}
 
 
 
